f50d95f173a04ac632cd7c50c402672e3fea833a
geekAtique <maiatikhoon@gmail.com>
2024-02-13 16:26:48 +0530

    fourth commit

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ console.log("this is my modified line") ;
 function hello() { 
      console.log("Hello from hello function") ; 
 }
+
+function play() {
+
+     console.log("this is a play function defined") ; 
+}
